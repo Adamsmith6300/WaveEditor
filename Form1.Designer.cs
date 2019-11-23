eddeae283927faa1ca57feefe1183f870e9ace87
Adam <Adamsmith6300@gmail.com>
2019-11-23 13:13:06 -0800

    before windowing

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -49,11 +49,11 @@
             this.filterButton = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
@@ -204,7 +204,7 @@
             // 
             // dftButton
             // 
-            this.dftButton.Location = new System.Drawing.Point(17, 35);
+            this.dftButton.Location = new System.Drawing.Point(100, 157);
             this.dftButton.Name = "dftButton";
             this.dftButton.Size = new System.Drawing.Size(108, 38);
             this.dftButton.TabIndex = 9;
@@ -238,15 +238,26 @@
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.label1);
             this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.comboBox2);
             this.groupBox2.Controls.Add(this.comboBox1);
             this.groupBox2.Controls.Add(this.dftButton);
             this.groupBox2.Location = new System.Drawing.Point(828, 325);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(397, 100);
+            this.groupBox2.Size = new System.Drawing.Size(320, 214);
             this.groupBox2.TabIndex = 9;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Discrete Fourier";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 17);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Performance";
             // 
             // comboBox1
             // 
@@ -255,22 +266,29 @@
             "Regular",
             "Regular Performance Test",
             "MMX Performance Test"});
-            this.comboBox1.Location = new System.Drawing.Point(151, 43);
+            this.comboBox1.Location = new System.Drawing.Point(17, 43);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(226, 24);
+            this.comboBox1.Size = new System.Drawing.Size(284, 24);
             this.comboBox1.TabIndex = 10;
             // 
             // groupBox3
             // 
-            this.groupBox3.Controls.Add(this.label1);
-            this.groupBox3.Controls.Add(this.comboBox2);
             this.groupBox3.Controls.Add(this.filterButton);
-            this.groupBox3.Location = new System.Drawing.Point(828, 473);
+            this.groupBox3.Location = new System.Drawing.Point(828, 563);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(397, 110);
+            this.groupBox3.Size = new System.Drawing.Size(151, 110);
             this.groupBox3.TabIndex = 12;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Filtering";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 17);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Windowing";
             // 
             // comboBox2
             // 
@@ -279,28 +297,10 @@
             "Rectangular",
             "Triangular",
             "Hamming"});
-            this.comboBox2.Location = new System.Drawing.Point(151, 47);
+            this.comboBox2.Location = new System.Drawing.Point(17, 110);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(226, 24);
+            this.comboBox2.Size = new System.Drawing.Size(284, 24);
             this.comboBox2.TabIndex = 13;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(148, 27);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(76, 17);
-            this.label1.TabIndex = 14;
-            this.label1.Text = "Windowing";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(148, 23);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(89, 17);
-            this.label2.TabIndex = 13;
-            this.label2.Text = "Performance";
             // 
             // Form1
             // 
@@ -327,7 +327,6 @@
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             this.groupBox3.ResumeLayout(false);
-            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
